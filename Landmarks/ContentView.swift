33054d8ca81cd86bef6b247b91450d29694af861
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-03-30 00:25:48 -0500

    First Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Rigo Hernandez on 3/30/20.
+//  Copyright © 2020 Rigo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Rigo!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
