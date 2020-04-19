83d37a8c0fbb7fa1e5e18ab911afe140aa7abd86
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-04-18 23:32:10 -0500

    Use an observable object for storage

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Rigo Hernandez on 4/18/20.
+//  Copyright © 2020 Rigo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+    
+}
