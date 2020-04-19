0053201b9f7ed03e9ef7516b2fff59486b956f0f
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-04-19 00:24:52 -0500

    Create Badge View

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Rigo Hernandez on 4/19/20.
+//  Copyright © 2020 Rigo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
