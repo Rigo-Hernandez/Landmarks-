1f219f929410fc9c56713873b21f8dd7fbeb8fca
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-04-18 23:50:00 -0500

    Adopt the model object in views

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -9,19 +9,23 @@
 
 import SwiftUI
 
+
 struct LandmarkList: View {
-    @State var showFavoritesOnly = true
-    
+    @EnvironmentObject var userData: UserData
+
+
     var body: some View {
         NavigationView {
-            List{
-                Toggle(isOn: $showFavoritesOnly){
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                ForEach(landmarkData)  {landmark in
-                if !self.showFavoritesOnly || landmark.isFavorite {
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+
+
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
                         }
                     }
                 }
@@ -31,12 +35,10 @@ struct LandmarkList: View {
     }
 }
 
+
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
+            .environmentObject(UserData())
     }
 }
