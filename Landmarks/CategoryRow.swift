09044548df8ac277e1e477707fbc24ed4628f731
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-04-23 23:26:19 -0500

    Composing the home view

--- Landmarks/CategoryRow.swift
+++ Landmarks/CategoryRow.swift
@@ -12,25 +12,41 @@ struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
     
-
-        var body: some View {
-            VStack(alignment: .leading) {
-                Text(self.categoryName)
-                    .font(.headline)
-                    .padding(.leading, 15)
-                    .padding(.top, 5)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(alignment: .top, spacing: 0) {
-                        ForEach(self.items) { landmark in
-                            Text(landmark.name)
-                        }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { landmark in
+                        CategoryItem(landmark: landmark)
                     }
                 }
-                .frame(height: 185)
             }
+            .frame(height: 185)
         }
     }
+}
+
+struct CategoryItem: View{
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading){
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
 
 struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
