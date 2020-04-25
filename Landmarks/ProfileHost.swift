1b0ce7a92ea4d63da911ca5452e5757f9c9faa71
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-04-24 21:08:03 -0500

    Created Profile view

---
+++ Landmarks/ProfileHost.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Rigo Hernandez on 4/24/20.
+//  Copyright © 2020 Rigo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
