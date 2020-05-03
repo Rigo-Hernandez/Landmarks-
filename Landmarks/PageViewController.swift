595f7c4b821e5c9c6d77a7710603f223b96f7112
Rigo Hernandez <hernandez_rigoberto@outlook.com>
2020-05-02 22:57:58 -0500

    Creating View to represent a UIPageViewController

---
+++ Landmarks/PageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Rigo Hernandez on 5/2/20.
+//  Copyright © 2020 Rigo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+}
